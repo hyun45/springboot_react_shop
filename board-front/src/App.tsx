a04c8c1729d4e874abeda26c6f2989d7d17f1023
hyun45 <sk86529743@gmail.com>
2024-03-21 22:58:04 +0900

    라우터 설정

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,32 +1,42 @@
-import React, { useState } from 'react';
+import Main from 'views/Main';
 import './App.css';
-import BoardItem from 'components/BoardItem';
-import { favoriteListMock, latestBoardListMock, replyListMock, top3BoardListMock } from 'mocks';
-import Top3Item from 'components/Top3Item';
-import ReplyItem from 'components/ReplyItem';
-import FavoriteItem from 'components/FavoriteItem';
-import InputBox from 'components/InputBox';
-import Footer from 'layouts/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Authentication from 'views/Authentication';
+import User from 'views/User';
+import Search from 'views/Search';
+import BoardWrite from 'views/Board/Write';
+import BoardDetail from 'views/Board/Detail';
+import BoardUpdate from 'views/Board/Update';
+import Container from 'layouts/Contatiner';
+import { AUTH_PATH, BOARD_DETAIL_PATH, BOARD_PATH, BOARD_UPDATE_PATH, BOARD_WRITE_PATH, MAIN_PATH, SEARCH_PATH, USER_PATH } from 'constant';
 
+
+// component: Application 컴포넌트
 function App() {
 
-  const [value, setValue] = useState<string>('');
-
+// rendering: Application 컴포넌트 렌더링
+// description: 메인화면 : '/' - Main
+// description: 로그인, 회원가입 : '/auth' - Authentication
+// description: 유저 페이지 : '/user/:userEmail' - User
+// description: 검색 화면 : '/search/:searchWord' - Search
+// description: 게시글 작성 : '/board/write' - BoardWrite
+// description: 게시글 상세 : '/board/:boardNumber' - BoardDetail
+// description: 게시글 수정 : '/board/update/:boardNumber' - BoardUpdate
   return (
-    <>
-      <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요.' value={value} error={false} setValue={setValue} />
-      <div style={{display: 'flex', columnGap: '30px', rowGap: '20px'}}>
-        {favoriteListMock.map(favoriteListItem => <FavoriteItem favoriteListItem={favoriteListItem} />)}
-      </div>
-      <div style={{padding: '0 20px', display: 'flex', flexDirection: 'column', gap: '30px'}}>
-        {replyListMock.map(replyListItem => <ReplyItem replyListItem={replyListItem} />)}
-      </div>
-      <div style={{display: 'flex', justifyContent: 'center', gap: '14px'}}>
-        {top3BoardListMock.map(top3ListItem => <Top3Item top3ListItem={top3ListItem} />)}
-      </div>
-      {latestBoardListMock.map(boardListItem => <BoardItem boardListItem={boardListItem} />)}
-      <Footer />
-    </>
+    <Routes>
+      <Route element={<Container />}>
+        <Route path={MAIN_PATH()} element={<Main />}/>
+        <Route path={AUTH_PATH()} element={<Authentication />}/>
+        <Route path={USER_PATH(':userEmail')} element={<User />}/>
+        <Route path={SEARCH_PATH(':searchWord')} element={<Search />}/>
+        <Route path={BOARD_PATH()}>
+          <Route path={BOARD_WRITE_PATH()} element={<BoardWrite />}/>
+          <Route path={BOARD_DETAIL_PATH(':boardNumber')} element={<BoardDetail />}/>
+          <Route path={BOARD_UPDATE_PATH(':boardNumber')} element={<BoardUpdate />}/>
+        </Route>
+        <Route path='*' element={<h1>404 NOT FOUND</h1>} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ board-front/src/views/Board/Write/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+// component: 게시글 작성 화면 컴포넌트
+export default function BoardWrite() {
+
+// render: 게시글 작성 화면 컴포넌트 렌더링
+    return (
+        <div>게시글 작성 페이지</div>
+    )
+}
