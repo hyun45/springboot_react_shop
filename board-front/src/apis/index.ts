7a520d5c1cdf09ff6f4a86ef9780b1912d21d579
hyun45 <sk86529743@gmail.com>
2024-03-26 13:44:59 +0900

    게시글 작성

--- board-front/src/apis/index.ts
+++ board-front/src/apis/index.ts
@@ -3,6 +3,8 @@ import { LoginRequestDto, SignUpRequestDto } from './request/auth';
 import { LoginResponseDto, SignUpResponseDto } from './response/auth';
 import { ResponseDto } from './response';
 import { GetLoginUserResponseDto } from './response/user';
+import { PostBoardRequestDto } from './request/board';
+import { PostBoardResponseDto } from './response/board';
 
 const DOMAIN = 'http://localhost:4000';
 
@@ -41,6 +43,23 @@ export const signUpRequest = async (requestBody: SignUpRequestDto) => {
     return result;
 }
 
+const POST_BOARD_URL = () => `${DOMAIN}/board`;
+
+export const postBoardRequest = async (requestBody: PostBoardRequestDto, accessToken: string) => {
+    const result = await axios.post(POST_BOARD_URL(), requestBody, authorization(accessToken))
+        .then(response => {
+            const responseBody: PostBoardResponseDto = response.data;
+            return responseBody;
+        })
+        .catch(error => {
+            if(!error.response) return null;
+            const responseBody: ResponseDto = error.response.data;
+            return responseBody;
+        })
+
+    return result;
+}
+
 const Get_Login_USER_URL = () => `${DOMAIN}/user`;
 
 export const getLoginUserRequest = async (accessToken: string) => {
@@ -55,6 +74,25 @@ export const getLoginUserRequest = async (accessToken: string) => {
             const responseBody: ResponseDto = error.response.data;
             return responseBody;
         });
+
+    return result;
+}
+
+const File_DOMAIN = `${DOMAIN}/file`;
+
+const FILE_UPLOAD_URL = () => `${File_DOMAIN}/upload`;
+
+const multipartFormData =  { headers: {'Content-Type': 'multipart/form-data'} };
+
+export const fileUploadRequest = async (data: FormData) => {
+    const result = await axios.post(FILE_UPLOAD_URL(), data, multipartFormData)
+        .then(response => {
+            const responseBody: string = response.data;
+            return responseBody;
+        })
+        .catch(error => {
+            return null;
+        })
 
     return result;
 }
