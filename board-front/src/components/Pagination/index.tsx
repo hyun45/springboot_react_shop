6a88eb2a8ad8177d7ba20773a940dbd046661bbe
hyun45 <sk86529743@gmail.com>
2024-03-28 10:07:40 +0900

    페이지네이션 수정

--- board-front/src/components/Pagination/index.tsx
+++ board-front/src/components/Pagination/index.tsx
@@ -26,16 +26,14 @@ export default function Pagination(props: Props) {
 // event handler: 이전 클릭 이벤트 처리
     const onPreviousClickHandler = () => {
         if(currentSection === 1) return;
-        setCurrentPage((currentSection - 1) * 5);
-        // setCurrentPage((currentSection - 1) * 10);
+        setCurrentPage((currentSection - 1) * 10);
         setCurrentSection(currentSection - 1);
     };
 
 // event handler: 다음 클릭 이벤트 처리
     const onNextClickHandler = () => {
         if(currentSection === totalSection) return;
-        setCurrentPage(currentSection * 5 + 1);
-        // setCurrentPage(currentSection * 10 + 1);
+        setCurrentPage(currentSection * 10 + 1);
         setCurrentSection(currentSection + 1);
     };
 
