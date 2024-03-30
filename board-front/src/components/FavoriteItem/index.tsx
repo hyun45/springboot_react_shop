8a2aa39844cb4a0fccd3c7c3e936cdb6c7a3d9c6
hyun45 <sk86529743@gmail.com>
2024-03-30 12:00:14 +0900

    favoriteItem 테일윈드 적용

--- board-front/src/components/FavoriteItem/index.tsx
+++ board-front/src/components/FavoriteItem/index.tsx
@@ -14,12 +14,20 @@ export default function FavoriteItem({ favoriteListItem }: Props) {
     const { profileImage, nickname } = favoriteListItem;
 
 // render: Favorite List Item 렌더링
+    // return (
+    //     <div className='favorite-list-item'>
+    //         <div className='favorite-list-item-profile-box'>
+    //             <div className='favorite-list-item-profile-image' style={{backgroundImage: `url(${profileImage ? profileImage : defaultProfileImage})`}}></div>
+    //         </div>
+    //         <div className='favorite-list-item-nickname'>{nickname}</div>
+    //     </div>
+    // )
     return (
-        <div className='favorite-list-item'>
-            <div className='favorite-list-item-profile-box'>
-                <div className='favorite-list-item-profile-image' style={{backgroundImage: `url(${profileImage ? profileImage : defaultProfileImage})`}}></div>
+        <div className='flex items-center gap-[6px]'>
+            <div className='w-[30px] h-[30px]'>
+                <div className='rounded-50% w-full h-full bg-50% bg-100%' style={{backgroundImage: `url(${profileImage ? profileImage : defaultProfileImage})`}}></div>
             </div>
-            <div className='favorite-list-item-nickname'>{nickname}</div>
+            <div className='text-black text-opacity-60 text-[16px] font-medium leading-140%'>{nickname}</div>
         </div>
     )
 }
